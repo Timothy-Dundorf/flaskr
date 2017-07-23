ea0baafe653d088b80ff0400688b4285ddfc4301
Timothy Dundorf <dundorft@vt.edu>
2017-07-23 17:04:50 -0400

    Spelling fix

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -9,7 +9,7 @@ app.config.from_object(__name__) # load config from this file, flaskr.py
 
 #Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABADE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
